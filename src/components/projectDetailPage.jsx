4dc011c04474489ed5d4359917a2678c4d1214c7
Aria Hemin <ariahemin@gmail.com>
2023-01-26 06:34:35 +0300

    made it more responosve

--- src/components/projectDetailPage.jsx
+++ src/components/projectDetailPage.jsx
@@ -7,15 +7,15 @@ function ProjectDetailPage(props){
 
 
     return(
-        <div className=" w-[800px]  flex flex-col">
+        <div className=" sm:w-[800px]  flex flex-col">
             <div>
                 {props.project.detail}
             </div>
             <div>{ typeof props.project.image !== "string" ? 
-            <div className="flex flex-row">
-                <img className="w-1/3" src={props.project.image[0]} alt={"image of apps"} />
-                <img className="w-1/3" src={props.project.image[1]} alt={"image of apps"} />
-                <img className="w-1/3" src={props.project.image[2]} alt={"image of apps"} />
+            <div className="flex flex-col sm:flex-row">
+                <img className="sm:w-1/3" src={props.project.image[0]} alt={"image of apps"} />
+                <img className="sm:w-1/3" src={props.project.image[1]} alt={"image of apps"} />
+                <img className="sm:w-1/3" src={props.project.image[2]} alt={"image of apps"} />
             </div> :  <img className="w-64" src={props.project.image} alt={"image of apps"}/>}
             </div>
         </div>
